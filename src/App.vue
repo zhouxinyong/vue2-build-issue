95e7d69cba15032357cdb6c0813db24aaa3a44df
zhouxinyong <zhoudaxia5683@163.com>
2020-12-17 17:49:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" v-if="list.includes(4)"/>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      list: [1, 2, 3]
+    }
   }
 }
 </script>
